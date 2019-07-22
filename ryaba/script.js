1bdc81052fac0add22e1f6e2add4ea7311801f51
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-07-22 19:15:13 +0300

    ryaba release 1.0

--- ryaba/script.js
+++ ryaba/script.js
@@ -1,32 +1,88 @@
 const ryabaLink = 'https://api.myjson.com/bins/jcmhn';
 const aboutText = 'Домашнее задание по модулю 5';
 let story = '';
+let res = '';
+const $result = $('.result');
+const $spinner = $('.spinner-border');
+let count = 0;
 
-function getRyabaStory(link) {
+const varr = [
+    'var1',
+    'var2',
+    'var3',
+    'var4',
+    'var5',
+    'var6',
+    'speach'
+]
+
+function getRyabaJSON(link) {  // Получаем заготовку сценария
     $.getJSON(link, function() {
         console.log("success");
     })
         .done(function(data) {
             console.log('success get');
             console.log(data);
-            story = data;
-            return story;
+            story = data['text'];
+            story = var2span(getRyabaStory(story), varr);
+            $result.html(story);
+            readVal(varr);
+            despin();
         })
         .fail(function() {
             console.log('error get');
-            return 'ошибка получения JSON'
+            story = 'ERROR getting JSON!!!';
+            $result.html(story);
+            despin();
         });
 };
 
-function getStory(link) {
-    $.getJSON(link)
-    .done(function(answer) {
-        console.log(answer)
-    }
-    )
+
+
+function getRyabaStory(arr) {
+    let text = '';
+    for(let i = 0; i < arr.length; i++) {
+        text = text + '<p>' + arr[i] + '</p>';
+    };
+    return text;
 }
 
-let ryabaStory = getRyabaStory(ryabaLink);
+function var2span(text, arr) {
+    let oldStr = '';
+    let newStr = '';
+    for(let i = 0; i < arr.length; i++) {
+        oldStr = new RegExp('{' + arr[i] + '}', 'g');
+        newStr = '<span class = "' + arr[i] + '">' + arr[i] + '</span>'
+        text = text.replace(oldStr, newStr);
+    };
+    return text;
+};
+
+
+function readVal(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        $('.' + arr[i]).html($('#'+arr[i]).val());
+    };
+};
+
+function despin() {
+    $spinner.css("display", "none");
+};
+
+function spin() {
+    $spinner.css("display", "inline-block");
+};
+
+function testTimeout() {
+    spin();
+    setTimeout(function() {despin()}, 2000);
+}
+
+function redFlash(jqObj) {
+    for(let i = 255; i > 0; i--) {
+        jqObj.css('background-color', 'rgb(255, ' + i +', ' + i +')');
+    };
+}
 
 function returnAbout(text) {
     console.log(text)
@@ -35,22 +91,21 @@ function returnAbout(text) {
 };
 
 
+
+
 $(document).ready(function() {
-    console.log('');
+    console.log('document ready');
 
     $("#about").click(function() {
         returnAbout(aboutText);
     });
     
     $(".btn").click(function() {
-        getRyabaStory(ryabaLink);
+        console.log('button clicked');
+        spin();
+        getRyabaJSON(ryabaLink);
+
     });
     
     }
 );
-
-
-/*
-$('.btn').click(testReturn());
-*/
-
