0d29719640d14d78cec952b497aadbb9e7fbb621
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-07-31 14:49:11 +0300

    release hw.b.1

--- hw.b.1.14/hw.py
+++ hw.b.1.14/hw.py
@@ -1,44 +1,40 @@
-flowers = {
-    "iris_setosa": {
-        "sepal_length": [3.6, 4.9, 4.8, 4.7],
-        "sepal_width": [2.9, 3.3, 3.2, 3.1],
-        "petal_length": [1.3, 1.2, 1.5, 1.4],
-    },
-    "iris_virginica": {
-        "sepal_length": [7.2, 7.0, 7.9],
-         "sepal_width": [3.1, 2.7, 2.8],
-        "petal_length": [5.5, 5.5, 6.5],
-    },
-    "iris_versicolor": {
-        "sepal_length": [6.5, 6.0, 6.1, 6.2, 6.3],
-         "sepal_width": [2.8, 2.9, 2.4, 2.7, 2.7],
-        "petal_length": [4.8, 4.7, 5.0, 4.9, 4.8],
-    },
-}
+from events import events
 
-def get_mean_sepal_length(flowers):
-    for flower in flowers:
-        print(keys(flower))
-    pass
+def getEventsCount(events, eventType):
+    count = 0
+    for event in events:
+            if event['eventType'] == eventType:
+                count += 1
+    return count
 
-def get_mean_sepal_width(flowers):
-    pass
+def getMinTimestampEvent(events):
+    minTS = events[0]['timestamp']
+    sessId = events[0]['sessionId']
+    for event in events:
+        if event['timestamp'] < minTS:
+            minTS = event['timestamp']
+            sessId = event['sessionId']
+    return [minTS, sessId]
 
-def get_mean_petal_length(flowers):
-    pass
+def getSuvItemPrice(events):
+    sumPrice = 0
+    for event in events:
+        sumPrice += event['item_price']
+    return sumPrice
 
-# выше были данные, а после этой строчки
-# вам надо дописать код.
+def getFirstSession(events):
+    for event in events:
+        if event['firstInSession']:
+            return event['pageViewId']
+        else:
+            return None
 
-# общее среднее значение для sepal_length:
-mean_sepal_length = get_mean_sepal_length(flowers)
-print(mean_sepal_length)
-# общее среднее значение для sepal_width:
-mean_sepal_width = get_mean_sepal_width(flowers)
-print(mean_sepal_width)
-# общее среднее значение для petal_length:
-mean_petal_length = get_mean_petal_length(flowers)
-print(mean_petal_length)
+print(getEventsCount(events, 'itemViewEvent'))
+print(getEventsCount(events, 'itemBuyEvent'))
 
+print(getEventsCount(events, 'detectedDuplicate'))
+print(getMinTimestampEvent(events))
 
+print(getSuvItemPrice(events))
 
+print(getFirstSession(events))
