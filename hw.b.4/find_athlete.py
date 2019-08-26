a678f091730108490c657f5faaff171de2b05d5f
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-08-26 16:02:44 +0300

    add some comment

--- hw.b.4/find_athlete.py
+++ hw.b.4/find_athlete.py
@@ -41,9 +41,13 @@ def connect_db(DB_PATH):
     return session()
 
 '''
-Поиск пользователя в таблицу user
-'''
+Поиск пользователя в таблицу user.
+Ищем сначала по id, потом по связке имя/фимилия, потом отдельно по имени или по фамилии.
+Возвращаем экземпляр User или None, если юзера не нашли.
 
+Для раскрытия функционала этой функции хорошо бы доделать правильный парсинг одной строки из input, 
+а не тупой построчный опрос...
+'''
 def find_user(session, first_name = '', last_name = '', u_id = ''):
     if isinstance(u_id, int):
         query = session.query(User).filter(User.id == u_id)
@@ -63,16 +67,23 @@ def find_user(session, first_name = '', last_name = '', u_id = ''):
             return query.first()
     return None
 
-#Найдем ближайшего спортсмена по росту
-#Делаем два запроса:
-#   -забираем всех кто ниже, сортируем от большего к меньшему, забираем первого
-#   -берем всех, кто выше, сортируем от меньшего к большему, забираем первого
-#   сравниваем
-#   На всякий случай цепляем число таких спрортсменов
+'''
+Найдем ближайшего спортсмена по росту
+Делаем два запроса:
+   -забираем всех кто ниже, сортируем от большего к меньшему, забираем первого
+   -берем всех, кто выше, сортируем от меньшего к большему, забираем первого
+   сравниваем
+
+На всякий случай цепляем число таких спрортсменов. В дальшейшем можно при выводе указывать это число и
+по запросу выводить список с одинаковым ростом.
+'''
 def find_nearest_height(session, height):
+    #Нормализация данных из разных таблиц. В user рост в сантиметрах, в athelete в метрах
     height = height / 100
+
     athlete1 = session.query(Athlete).filter(Athlete.height <= height).order_by(Athlete.height.desc()).first()
     athlete2 = session.query(Athlete).filter(Athlete.height >= height).order_by(Athlete.height.asc()).first()
+
     if athlete1 is None:
         athlete = athlete2
     elif athlete2 is None:
@@ -81,20 +92,25 @@ def find_nearest_height(session, height):
         athlete = athlete1
     else:
         athlete = athlete2
+
     count = session.query(Athlete).filter(Athlete.height == athlete.height).count()
     return {'athlete':athlete, 'count':count}
 
-#Найдем ближайшего спортсмена по возрасту
+
 '''
+Найдем ближайшего спортсмена по возрасту
+
 Повторяем предыдущую функцию.
-Надо бы их совместить, но пока не научилсяс передавать из объявления функции во внутрь типа:
+Надо бы их совместить, но пока я не научился передавать аргументы из объявления функции во внутрь типа:
 def function(session, field, value):
     x = sesseion.qyery(DBName).fidrer(field <= value).order_by(field.asc()).first()
     .........
 
-С другой стороны - при поиске даты рождения нужно выполнить более сложное приведение типов.
+С другой стороны - при поиске даты рождения нужно выполнить более другое приведение типов.
 '''
 def find_nearest_age(session, birthdate):
+
+    #функция формирования struct_time из строки по шаблону.
     st = lambda bd: mktime(dt.strptime(bd, "%Y-%m-%d").timetuple())
 
     birthdate = birthdate.strftime("%Y-%m-%d")
@@ -110,11 +126,6 @@ def find_nearest_age(session, birthdate):
         athlete = athlete2
     count = session.query(Athlete).filter(Athlete.birthdate == athlete.birthdate).count()
     return {'athlete':athlete, 'count':count}
-
-#для тестов
-def print_query(query):
-    for record in query:
-        print(record)
 
 
 def main():
@@ -134,10 +145,12 @@ def main():
     if desired_user is None:
         print('Такой пользователь не найден')
         return None
-    else:
-        print(f'\n{desired_user.first_name} {desired_user.last_name}. День рождения - {desired_user.birthdate.date()}. Рост - {desired_user.height / 100}\n')
-        athlete1 = find_nearest_height(session, desired_user.height)['athlete']
-        athlete2 = find_nearest_age(session, desired_user.birthdate)['athlete']
+    
+    print(f'\n{desired_user.first_name} {desired_user.last_name}. День рождения - {desired_user.birthdate.date()}. Рост - {desired_user.height / 100}\n')
+
+    athlete1 = find_nearest_height(session, desired_user.height)['athlete']
+    athlete2 = find_nearest_age(session, desired_user.birthdate)['athlete']
+    
     print('Ближайший атлет по росту:')
     print(f'{athlete1.name}. День рождения - {athlete1.birthdate}. Рост - {athlete1.height}\n')
     print('Ближайший атлет по возрасту')
