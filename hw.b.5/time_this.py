160d97e5c48b41c0b60ded6d37b97ac3c2cd7a8b
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-08-30 17:57:39 +0300

    release

--- hw.b.5/time_this.py
+++ hw.b.5/time_this.py
@@ -1,3 +1,5 @@
+NUM_RUNS = 1000000
+
 def time_this(num_runs):
     from time import time
     def decorator(func):
@@ -5,29 +7,60 @@ def time_this(num_runs):
             avg_time = 0
             for _ in range(num_runs):
                 t0 = time()
-
-                func(*args, **kwargs)
-                
+                result = func(*args, **kwargs)
                 t1 = time()
                 avg_time += (t1 - t0)
             avg_time /= num_runs
             print("Выполнение функции %s%s заняло %.2f секунд" % (func.__name__, args, avg_time))
-            return "Выполнение функции %s%s заняло %.2f секунд" % (func.__name__, args, avg_time)
+            return result
         return wrap
     return decorator
-
 
 @time_this(num_runs = 10)
 def f1():
     for _ in range(2000000):
         pass
+    return "Закончили процедуры"
 
 @time_this(num_runs = 10)
 def f2(iterations):
     for _ in range(iterations):
         pass
+    return "Закончили процедуры"
+
+def f3(iterations):
+    for _ in range(iterations):
+        pass
+    return "Закончили процедуры"
+'''
+deco = time_this(10)
+f3 = deco(f3)
+'''
+
+def main():
+    f1()
+    f2(2000000)
+    f3(1000000)
+
+if __name__ == "__main__":
+    deco = time_this(10)
+    f3 = deco(f3)
+    main()
 
 
-f1()
 
-f2(10000000)
+'''
+
+new_decorator = time_this(num_runs = NUM_RUNS)
+
+f4 = new_decorator(f3)
+
+f4(100000)
+
+f = time_this()
+
+'''
+
+#f(10000000) это эквивалент time_this(num_run = 10)(f)(1000000)
+
+
